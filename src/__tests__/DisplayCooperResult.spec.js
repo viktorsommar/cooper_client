3b3260b565058146533945526503d6ab979fe407
felixoppongwillborg <felixwillborg@MacBook-Pro.local>
2020-07-01 14:55:59 +0200

    created the cooper calculator all tests pass

--- src/__tests__/DisplayCooperResult.spec.js
+++ src/__tests__/DisplayCooperResult.spec.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import DisplayCooperResault from "../components/DisplayCooperResault";
+import DisplayCooperResult from "../components/DisplayCooperResult";
 
 describe("<DisplayCooperResult />", () => {
   let describedComponent;
-  describe("evaluates the correct result for femail/poor", () => {
+  describe("evaluates the correct result for female/poor", () => {
     beforeAll(() => {
       describedComponent = shallow(   
-        <DisplayCooperResault distance="1000" gender="female" ange="23" />
+        <DisplayCooperResult distance="1000" gender="female" age="23" />
       );
     });
 
     it("and returns the assessment", () => {
       expect(describedComponent.find("p#cooper-result").text()).toEqual("Result: Poor");
+      debugger
     });
 
     it("and returns the data user put in", () => {
@@ -26,12 +27,12 @@ describe("<DisplayCooperResult />", () => {
   describe("evaluates the correct result for female/average", () => {
     beforeAll(() => {
       describedComponent = shallow(
-        <DisplayCooperResault distance="2000" gender="female" age="23" />
+        <DisplayCooperResult distance="2000" gender="female" age="23" />
       );
     });
 
     it("and returns the assessment", () => {
-      expect(describedComponent.find("p#cooper-result").text()).toEqual("Result: Avergae");
+      expect(describedComponent.find("p#cooper-result").text()).toEqual("Result: Average");
     });
 
     it("and returns the data user put in", () => {
