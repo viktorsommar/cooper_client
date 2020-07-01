dd1bb0f9197212e7442fa468f568c6eb5b9faada
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-01 16:52:37 +0200

    Adding axios, continuing with login logic, adapting app jsx to new code.

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,62 @@ import React, { Component } from 'react';
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from './components/InputFields';
 import LoginForm from "./components/LoginForm";
+import { authenticate } from "./modules/auth";
 
 class App extends Component {
   state = {
     distance: "",
     gender: "female",
-    age: ""
-  }
+    age: "",
+    renderLoginForm: false,
+    authenticated: false,
+    message: ""
+  };
 
   onChangeHandler = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  render() {
-    const renderLogin = this.state.renderLoginForm ? (
-      <LoginForm />
-    ) : (
-      <button 
-      id="login" 
-      onClick={() => this.setState({ renderLoginForm: true })}
-      >
-      Login
-      </button>
+  onLogin = async e => {
+    e.preventDefault();
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
     );
+    if (response.authenticated) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false });
+    }
+  };
+
+  render() {
+    const { renderLoginForm, authenticated, message } = this.state;
+    let renderLogin;
+    switch(true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />
+        break;
+      case !renderLoginForm && !authenticated:
+        renderLogin = (
+          <>
+            <button
+              id="login"
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >
+              Login
+            </button>
+            <p>{message}</p>
+          </>
+        );
+        break;
+      case authenticated:
+        renderLogin = (
+          <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+        );
+        break;
+    }
+
     return (
       <>
       <InputFields onChangeHandler={this.onChangeHandler} />
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+axios.defaults.baseURL = "http://localhost:3000/api/v1"
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
